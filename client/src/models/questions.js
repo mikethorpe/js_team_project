a7b18bbfb0ddefa67559e5d907c9c63c39d44b1c
mikethorpe <michael.a.thorpe@gmail.com>
2018-09-21 18:13:01 +0100

    Adding solution to catch question data from multiple urls

--- client/src/models/questions.js
+++ client/src/models/questions.js
@@ -7,18 +7,42 @@ const Questions = function () {
         'https://opentdb.com/api.php?amount=4&difficulty=medium&type=multiple',
         'https://opentdb.com/api.php?amount=4&difficulty=hard&type=multiple'
     ]
-    this.questions = [];
+    this.questionsArray = [];
 }
 
+
 Questions.prototype.getData = function () {
+    
+    let numberOfApiRequests = 0;
+    PubSub.subscribe('Questions:api-response-received', () => {
+        numberOfApiRequests++
+        if (numberOfApiRequests > 2){
+            PubSub.publish('Questions:question-data-ready', this.questionsArray)
+        }
+    })
+
+    this.makeApiRequests();
+}
+
+Questions.prototype.makeApiRequests = function() {
     this.urls.forEach(url => {
         const request = new Request(url);
         request.get()
-            .then((questions) => {
-                console.log(questions);
+            .then((questionsResponse) => {
+                const questionsFromAPi = questionsResponse.results;
+                this.pushDataToQuestionsArray(questionsFromAPi);
+                PubSub.publish('Questions:api-response-received');
             })
             .catch(console.error);
     })
 }
+
+Questions.prototype.pushDataToQuestionsArray = function(questions) {
+    while (questions.length != 0) {
+        const question = questions.pop();
+        this.questionsArray.push(question);
+    }
+}
+
 
 module.exports = Questions;
