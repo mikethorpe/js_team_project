9ccdb7f3aec978f1bd5a62f5f3b746883f36e71d
mikethorpe <michael.a.thorpe@gmail.com>
2018-09-23 10:49:13 +0100

    Refactoring check answer logic into separate method in game model

--- client/src/models/game.js
+++ client/src/models/game.js
@@ -7,7 +7,6 @@ const Game = function() {
 }
 
 Game.prototype.bindEvents = function(){
-
     PubSub.subscribe('Questions:questions-data-ready', (event) => {
         this.questions = event.detail;
         this.nextQuestion();
@@ -15,17 +14,7 @@ Game.prototype.bindEvents = function(){
 
     PubSub.subscribe('AnswerView:answer-submitted', (event) => {
         const answerSubmitted = event.detail;
-        const correctAnswer = this.currentQuestion.correct_answer;
-
-        if (correctAnswer == answerSubmitted) {
-            console.log("correct answer");
-            this.nextQuestion();
-            // additional logic
-        }
-        else {
-            console.log('incorrect answer');
-            // end game logic
-        }
+        this.checkAnswer(answerSubmitted);
     })
 }
 
@@ -35,8 +24,18 @@ Game.prototype.nextQuestion = function(){
     PubSub.publish("Game:next-question-ready", this.currentQuestion);
 }
 
-Game.prototype.checkAnswer = function(){
+Game.prototype.checkAnswer = function(answerSubmitted){
+    const correctAnswer = this.currentQuestion.correct_answer;
 
+    if (correctAnswer == answerSubmitted) {
+        console.log("correct answer");
+        this.nextQuestion();
+        // additional logic
+    }
+    else {
+        console.log('incorrect answer');
+        // end game logic
+    }
 }
 
 module.exports = Game;
