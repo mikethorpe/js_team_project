70be0579b44496db68b98aa0abbefec98b4cc5ba
Ricardo Ruiz <rfpimpao@gmail.com>
2018-09-23 23:09:32 +0100

    established basic back-end server structure, no db yet

---
+++ server/helpers/create_router.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const createRouter = function () {
+
+    const router = express.Router();
+
+    //INDEX
+    router.get('/', (req, res) => {
+        
+    });
+
+    //SHOW
+
+    //CREATE
+
+    //UPDATE
+
+    //DELETE
+
+    return router;
+
+};
+
+module.exports = createRouter;
---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const parser = require('body-parser');
+const createRouter = require('./helpers/create_router');
+
+
+const publicPath = path.join(__dirname, '../client/public');
+app.use(express.static(publicPath));
+app.use(parser.json());
+
