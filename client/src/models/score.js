5888791748523a1fbfb3e0a4792ca573e2f9036c
mikethorpe <michael.a.thorpe@gmail.com>
2018-09-25 23:36:30 +0100

    Score now increments according to which crypto currency selected to play in

--- client/src/models/score.js
+++ client/src/models/score.js
@@ -4,14 +4,15 @@ const Score = function() {
     this.runningTotalGBP = 0;
     this.scoreOptionsArray = [];
     this.cryptoConversionFactors = null;
+    this.currentScoreOption = null;
 }
 
 const questionGbpValues = [
     500, 1000, 2000, 5000, 10000, 20000, 50000, 75000, 150000, 250000, 500000, 1000000
-    ];
+];
 
 Score.prototype.incrementScore = function() {
-    this.runningTotalGBP++;
+    this.runningTotalGBP += (this.currentScoreOption.amount * this.currentScoreOption.gbpConversionFactor);
     PubSub.publish('Score:score-updated', this.runningTotalGBP);
     console.log('Current running total score GBP: ', this.runningTotalGBP);
 }
@@ -20,6 +21,11 @@ Score.prototype.bindEvents = function() {
     PubSub.subscribe('CryptoCurrency:crypto-conversion-data-ready', (event) => {
         this.cryptoConversionFactors = event.detail;        
     })
+
+    PubSub.subscribe('ScoreOptionView:option-submitted', (event) => {
+        this.currentScoreOption = event.detail;
+    });
+
 }
 
 Score.prototype.resetScore = function() {
@@ -35,14 +41,20 @@ Score.prototype.createScoreOptions = function(currentQuestionNumber){
     const rippleObject = this.createScoreOption('Ripple', 'XRP', questionGbpValue);
     const scoreOptions = [bitcoinObject, etheriumObject, dogecoinObject, rippleObject];
     PubSub.publish('Score:score-options-for-question', scoreOptions);
+
+    //Bitcoin is the default score option
+    this.currentScoreOption = bitcoinObject;
 }
 
 Score.prototype.createScoreOption = function(currency, symbol, questionGbpValue){
     const amountConvertedToCrypto = this.convertCryptoScoreIntoGBP(symbol, questionGbpValue);
+
+    const cryptoConversionFactor = this.cryptoConversionFactors[symbol].GBP;    
     const optionObject = {
         amount: amountConvertedToCrypto,
         currency: currency,
-        symbol: symbol
+        symbol: symbol,
+        gbpConversionFactor: cryptoConversionFactor
     }
     return optionObject;
 }
